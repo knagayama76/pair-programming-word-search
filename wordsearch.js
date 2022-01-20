1ef263e0e15be2a0e1e909f2cf04e99af62eacf3
knagayama76 <knagayama76>
2022-01-19 19:03:12 -0500

    activity done

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
 
-module.exports = wordSearch
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
+
+// const transpose = function (matrix) {
+//   let newMatrix = [];
+//   let rows = matrix[0].length;
+//   let column = matrix.length;
+//   for (let i = 0; i < rows; i++) {
+//     let newRow = [];
+//     for (let j = 0; j < column; j++) {
+//       newRow.push(matrix[j][i]);
+//     }
+//     newMatrix.push(newRow);
+//   }
+//   return newMatrix;
+// };
+
+// transpose(letters);
